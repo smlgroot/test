11463396bf643f910e09e8be34c2f4a43a99e81b
smlg <smlgroot@gmail.com>
2012-06-27 19:55:59 -0500

    sencha framework

--- HTML5Test/src/com/smlg/test/Main.java
+++ HTML5Test/src/com/smlg/test/Main.java
@@ -1,21 +1,38 @@
 package com.smlg.test;
 
+import org.apache.cordova.DroidGap;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
 
-public class Main extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-       
-        WebView myWebView=(WebView) findViewById(R.id.myWebView);
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        
-        myWebView.loadUrl("file:///android_asset/www/index.html");
+public class Main extends DroidGap {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		/*
+		 * super.onCreate(savedInstanceState);
+		 * //requestWindowFeature(Window.FEATURE_NO_TITLE);
+		 * 
+		 * setContentView(R.layout.main);
+		 * 
+		 * WebView myWebView = (WebView) findViewById(R.id.webview);
+		 * myWebView.getSettings().setJavaScriptEnabled(true);
+		 * myWebView.setFocusableInTouchMode(false);
+		 * myWebView.setPadding(0,0,0,0);
+		 * myWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+		 * 
+		 * myWebView.loadUrl("file:///android_asset/www/index.html");
+		 * myWebView.addJavascriptInterface(new JavaScriptInterface(this),
+		 * "Android");
+		 */
 
-        
-    }
+		super.onCreate(savedInstanceState);
+		//super.setStringProperty("loadingDialog", "Starting your app...");
+		//super.setIntegerProperty("splashscreen", R.drawable.android);
+		//super.loadUrl("file:///android_asset/www/index.html", 5000);
+//		super.loadUrl("file:///android_asset/www/index.html");
+		//super.loadUrl("file:///android_asset/www/mobile_website/app.html");
+		super.loadUrl("file:///android_asset/www/production/index.html");
+	}
 }
